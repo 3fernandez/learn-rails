8c78b7dcb440526373e3142a020904efc24d1bc5
ecdfernandes <euclidesdef@hotmail.com>
2014-05-21 04:41:53 -0300

    Playing with Zurb Foundation Framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,11 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = "Welcome"
-		flash.now[:alert] = "My birthday is soon"
+		#flash.now[:notice] = "Welcome"
+		#flash.now[:alert] = "My birthday is soon"
+
+		#For dinamic update of date on footer
+		@displayDate = Date.today.year
 		#Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
 end
