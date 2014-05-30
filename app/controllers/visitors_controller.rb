24ec6f3b304400f9af965b7c3877f452fbca8502
ecdfernandes <euclidesdef@hotmail.com>
2014-05-30 07:49:52 -0300

    Mailing list feature implemented

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,25 @@
 class VisitorsController < ApplicationController
 	def new
-		@owner = Owner.new
+		@visitor = Visitor.new
 		#flash.now[:notice] = "Welcome"
 		#flash.now[:alert] = "My birthday is soon"
-
-		#For dinamic update of date on footer
-		@displayDate = Date.today.year
 		#Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
+
+	def create
+		@visitor = Visitor.new(secure_params)
+		if @visitor.valid?
+			@visitor.subscribe
+			flash[:notice] = "Signed up #{@visitor.email} successful."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+	def secure_params
+		params.require(:visitor).permit(:email)
+	end
+
 end
